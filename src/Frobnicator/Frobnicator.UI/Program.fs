d7ba333d8f6237d2685ede593b1b9a37a7b595fc
John Stovin <john.d.stovin@gmail.com>
2017-07-04 15:57:30 +0100

    Initial code

--- src/Frobnicator/Frobnicator.UI/Program.fs
+++ src/Frobnicator/Frobnicator.UI/Program.fs
@@ -1,7 +1,35 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace Frobnicator.UI
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+open Elmish
+open Elmish.WPF
+
+module Types =
+    type TextModel = {text : string}
+
+    type Msg = 
+    | Click
+
+module State =
+    open Types
+    
+    let init () = { text = "Initial Text" }
+
+    let update msg model = 
+        match msg with
+        | Click -> { model with text = "New Text" }
+
+module App = 
+    open Types
+    open State
+    
+    let view _ _ =
+        [ "Text" |> Binding.oneWay (fun m -> m.text)
+          "SetText" |> Binding.cmd (fun _ m -> Click) ]
+
+
+    [<EntryPoint; STAThread>]
+    let main argv = 
+        Program.mkSimple init update view
+        |> Program.withConsoleTrace
+        |> Program.runWindow (Frobnicator.Views.MainWindow())
